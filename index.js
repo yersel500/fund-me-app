411ee35c6115ad00fac53b7d0806cff2aecea0dd
yersel500 <yersel500@gmail.com>
2022-07-26 11:00:33 -0500

    add connect and fund functions

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
-    window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected";
+    await ethereum.request({ method: "eth_requestAccounts" });
+    connectButton.innerHTML = "Connected";
+    console.log(ethers);
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install metamask";
+    connectButton.innerHTML = "Please install metamask";
+  }
+}
+
+async function fund(ethAmount) {
+  console.log(`Funding with ${ethAmount}`);
+  if (typeof window.ethereum !== "undefined") {
   }
 }
